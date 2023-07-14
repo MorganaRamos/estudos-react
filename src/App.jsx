733c4caba8a8fbd41f0610116e771e33a4d8b0df
Morgana Ramos <morganaramos@edu.unifor.br>
2023-07-14 00:41:13 -0300

    atualizando projeto com array

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,67 @@
+import { useState } from "react"
+
 function App() {
-  
+  const [fields, setFields] = useState({
+    titulo:'',
+    sinopse:'',
+    votos:''
+  })
+ 
+  const adicionar = event =>{
+// evita que a página seja recarregada
+    event.preventDefault()
+    console.log("add", fields)
+ }
+
+  const atualizaInput = event =>{
+    console.log( event.target.id)
+    setFields({...fields, [event.target.id]:event.target.value})
+    // ... é para desestruturar o objeto, atualiza um objeto
+  }
+
+
+
   return (
     <>
       <div className="container">
         <h2 className="mt-3 mb-3">Cadastro de filmes</h2>
         <form  className="row g-3">
         <div className="col md-12">
-         <label htmlFor="" className="form-label">titulo</label>
-         <input type="text" className="form-control" />
+         <label htmlFor="titulo" className="form-label">titulo</label>
+         <input 
+         id="titulo" 
+         type="text" 
+         className="form-control" 
+         value={fields.titulo} 
+         onChange={atualizaInput}/>
         </div>
 
         <div className="col md-12">
-         <label htmlFor="" className="form-label">Sinopse</label>
-         <input type="text" className="form-control" />
+         <label htmlFor="sinopse" className="form-label">Sinopse</label>
+         <input 
+         id="sinopse" 
+         type="text" 
+         className="form-control" 
+         value={fields.sinopse} 
+         onChange={atualizaInput} />
         </div>
 
         <div className="col md-12">
-         <label htmlFor="" className="form-label">Votos</label>
-         <input type="text" className="form-control" />
+         <label htmlFor="votos" className="form-label">Votos</label>
+         <input 
+         id="votos" 
+         type="text" 
+         className="form-control" 
+         value={fields.votos} 
+         onChange={atualizaInput}/>
         </div>
-        <button className="btn btn-primary">Adicionar</button>
+        <button className="btn btn-primary" onClick={adicionar}>Adicionar</button>
         </form>
       </div>
     </>
   )
 }
+
+
 
 export default App
